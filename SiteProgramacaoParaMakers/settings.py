40df8199d3f236da5cd9a5b7fe45245a692f883c
rafaelchiafarelli <rafael.chiafarelli@gmail.com>
2019-01-09 13:15:52 -0200

    pswd present

--- SiteProgramacaoParaMakers/settings.py
+++ SiteProgramacaoParaMakers/settings.py
@@ -163,8 +163,8 @@ LOGIN_URL = 'login'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'programacaoparamakers@gmail.com'
-EMAIL_HOST_PASSWORD = 'Makers1234'
+EMAIL_HOST_USER = 'youtemail@gmail.com'
+EMAIL_HOST_PASSWORD = 'youpassowrd'
 EMAIL_PORT = 587
 
 
